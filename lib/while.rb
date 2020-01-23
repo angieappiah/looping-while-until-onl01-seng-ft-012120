03846bf190e47d8b370cc5812d31c6276d99f25d
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-23 02:47:22 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,11 @@
 def using_while
-  levitation_force = 6
-  counter = + 6
+  levitation_force = < 10
+  counter = + 1
 	phrase = Wingardium Leviosa
 	
-  # puts "Wingardium Leviosa # (counter)" .
-  while levitation_force >= 6
+loop do
+  # puts phrase"Wingardium Leviosa # (counter)" .
+  while levitation_force >= 1
 end
   
 end
